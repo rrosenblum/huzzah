795818a2b98823012cd805390978cea0ceb07936
Lance Howard <lance.howard@manheim.com>
2015-11-13 09:31:57 -0500

    passing role_data and browser to site instances

--- lib/huzzah/util/site_builder.rb
+++ lib/huzzah/util/site_builder.rb
@@ -1,40 +1,36 @@
 module Huzzah
   module SiteBuilder
-    include Browser
 
     private
 
+    def get_site_names
+      Huzzah::Page.subclasses.map(&:parent).uniq
+    end
+
     def define_sites!
+      @sites ||= {}
       get_site_names.each do |site|
-        next if site_defined?(site)
-        site.deconstantize.constantize.const_set(site.demodulize, Class.new(Huzzah::Site))
+        site_name = site.to_s.underscore.to_sym
+        @sites[site_name] = Huzzah::Site.new(site_name)
       end
     end
 
-    def get_site_names
-      Huzzah::Page.subclasses.map(&:to_s).map(&:deconstantize).flat_map { |m| "#{m}::#{m}" }.uniq
-    end
-
-    def site_defined?(site)
-      return true if site.constantize rescue NameError
-      false
-    end
-
-    def generate_site_methods!(role_data)
+    def generate_site_methods!
       define_sites!
-      @sites ||= {}
-      Huzzah::Site.subclasses.each do |site|
-        define_singleton_method(site.to_s.demodulize.underscore.to_sym) do
-          return @sites[site] unless @sites[site].nil? or browser_closed?
-          begin
-            @sites[site] = site.new(role_data, launch_browser)
-          rescue Errno::ECONNREFUSED
-            reset_browser
-            @sites = {}
-            retry
-          end
+      @sites.each_key do |site|
+        define_singleton_method(site) do |&block|
+          return_site(__method__.to_sym, &block)
         end
       end
+    end
+
+    def return_site(site_name, &block)
+      site = @sites[site_name]
+      site.role_data = @role_data
+      site.browser = @browser
+      site.visit
+      site.instance_eval(&block) if block_given?
+      site
     end
 
   end
