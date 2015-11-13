d2a6fdf28ac31bd8fd4964eabd72ab8aad2aabca
Lance Howard <lance.howard@manheim.com>
2015-11-13 09:28:25 -0500

    passing role_data and browser to page instances

--- lib/huzzah/util/page_builder.rb
+++ lib/huzzah/util/page_builder.rb
@@ -1,9 +1,27 @@
 module Huzzah
   module PageBuilder
+
     private
-    def generate_page_methods!(role_data, browser)
-      pages = Huzzah::Page.subclasses.select { |page| page.to_s.deconstantize == self.class.to_s.demodulize }
-      pages.each { |page| define_singleton_method(page.to_s.demodulize.underscore.to_sym) { page.new(role_data, browser) } }
+
+    def define_page_methods!
+      Huzzah::Page.subclasses.each do |subclass|
+        parent = subclass.parent.to_s.underscore.to_sym
+        page_name = subclass.to_s.demodulize.underscore.to_sym
+        if parent.eql? @site_name
+          define_singleton_method(page_name) do |&block|
+            return_page(subclass, &block)
+          end
+        end
+      end
     end
+
+    def return_page(subclass, &block)
+      page = subclass.new
+      page.role_data = @role_data
+      page.browser = @browser
+      page.instance_eval(&block) if block_given?
+      page
+    end
+
   end
 end
