d27919a31ee9a48de123f39dee79f7593b0a5f98
Lance Howard <lance.howard@manheim.com>
2015-11-13 09:19:12 -0500

    updating requires

--- lib/huzzah.rb
+++ lib/huzzah.rb
@@ -1,20 +1,22 @@
 require 'pry'
 require 'active_support/all'
 require 'yaml'
-require_relative 'huzzah/file_loader'
-require_relative 'huzzah/browser'
-require_relative 'huzzah/locator'
-require_relative 'huzzah/browseable'
-require_relative 'huzzah/site_builder'
-require_relative 'huzzah/page_builder'
-require_relative 'huzzah/partial_loader'
-require_relative 'huzzah/flow_builder'
-require_relative 'huzzah/role'
-require_relative 'huzzah/site'
-require_relative 'huzzah/partial'
-require_relative 'huzzah/flow'
-require_relative 'huzzah/page'
-require_relative 'huzzah/cucumber_dsl'
+
+require_relative 'huzzah/util/file_loader'
+require_relative 'huzzah/util/site_builder'
+require_relative 'huzzah/util/page_builder'
+require_relative 'huzzah/util/partial_builder'
+require_relative 'huzzah/util/flow_builder'
+require_relative 'huzzah/dsl/dsl'
+require_relative 'huzzah/dsl/cucumber_dsl'
+require_relative 'huzzah/core/browser'
+require_relative 'huzzah/core/locator'
+require_relative 'huzzah/core/base'
+require_relative 'huzzah/core/role'
+require_relative 'huzzah/core/flow'
+require_relative 'huzzah/core/site'
+require_relative 'huzzah/core/page'
+
 
 module Huzzah
 
@@ -29,6 +31,7 @@ module Huzzah
   class UndefinedPageError < StandardError; end
 
   class << self
+    include FileLoader
 
     attr_accessor :path, :environment, :default_driver
     attr_accessor :sites
@@ -43,6 +46,10 @@ module Huzzah
 
     def define_driver(name, &block)
       drivers[name] = block
+    end
+
+    def site_config(site_name)
+      load_config("#{Huzzah.path}/sites/#{site_name}.yml")
     end
 
   end
