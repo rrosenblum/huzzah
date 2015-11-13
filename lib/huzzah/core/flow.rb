0a60111cd259877badf5d6f46302902eefab9502
Lance Howard <lance.howard@manheim.com>
2015-11-13 09:20:34 -0500

    giving flows access to sites and flows

--- lib/huzzah/core/flow.rb
+++ lib/huzzah/core/flow.rb
@@ -1,27 +1,10 @@
 module Huzzah
-  class Flow
+  class Flow < Huzzah::Base
+    include DSL
 
-    def initialize(role)
-      @role = role
-    end
-
-    def method_missing(method_name, *args, &block)
-      if @role.respond_to?(method_name, false)
-        define_singleton_method(method_name) do |*args|
-          @role.send(method_name, *args, &block)
-        end
-        @role.send(method_name, *args, &block)
-      else
-        super
-      end
-    end
-
-    def respond_to?(method_name, include_private = false)
-      @role.respond_to?(method_name, include_private) || super
-    end
-
-    def respond_to_missing?(method_name, include_private = false)
-      @role.respond_to?(method_name, include_private) || super
+    def initialize
+      generate_site_methods!
+      generate_flow_methods!
     end
 
   end
