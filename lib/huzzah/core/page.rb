845f17517dd159c1127dbbc8043fe0883d355502
Lance Howard <lance.howard@manheim.com>
2015-11-18 11:26:05 -0500

    forward #p calls to browser

--- lib/huzzah/core/page.rb
+++ lib/huzzah/core/page.rb
@@ -1,7 +1,12 @@
+require 'forwardable'
+
 module Huzzah
   class Page < Huzzah::Base
     extend Locator
     extend PartialBuilder
+    extend Forwardable
+
+    def_delegators :browser, :p
 
   end
 end
--- lib/huzzah/version.rb
+++ lib/huzzah/version.rb
@@ -1,3 +1,3 @@
 module Huzzah
-  VERSION = '0.7.2'
+  VERSION = '0.7.3'
 end
