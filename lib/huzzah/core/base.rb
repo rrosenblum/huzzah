e1725c332c89904b70dd2ff4a944239e513ea129
Lance Howard <lance.howard@manheim.com>
2016-03-15 08:24:49 -0400

    combining browser & base classes

--- lib/huzzah/core/base.rb
+++ lib/huzzah/core/base.rb
@@ -1,8 +1,33 @@
 module Huzzah
   class Base
-    include Browser
 
-    attr_accessor :role_data, :browser
+    attr_accessor :role_data, :browser, :driver
+
+    private
+
+    ##
+    # Handles calls to methods in the Watir::Browser class so you do not have
+    # browser.method calls in your page object.
+    #
+    # For example you can just say:
+    #    text_field(id: 'username')
+    #        rather than
+    #    browser.text_field(id: 'username')
+    #
+    def method_missing(method_name, *args, &block)
+      return @browser.send(method_name, *args, &block) if @browser.respond_to?(method_name, false)
+      super
+    end
+
+    ##
+    # Launches a browser for the role. It uses the default driver
+    # unless a driver is specified in the role's YAML file.
+    #
+    def launch_browser
+      @driver = @role_data.key?(:driver) ? @role_data[:driver] : Huzzah.default_driver
+      fail Huzzah::DriverNotDefinedError, "Driver '#{@driver}' is not defined." unless Huzzah.drivers.key?(@driver)
+      @browser ||= Huzzah.drivers[@driver].call
+    end
 
   end
 end
