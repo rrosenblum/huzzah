d894b6865c6d89daf5ed4bd3e7e5f504f60c741f
Ryan Rosenblum <ryan.rosenblum@coxautoinc.com>
2019-04-26 10:49:31 -0400

    Fix the Google search button name and type

--- spec/examples/partials/google_search_form.rb
+++ spec/examples/partials/google_search_form.rb
@@ -2,7 +2,7 @@ module Google
   class SearchForm < Huzzah::Page
 
     locator(:keywords) { text_field(name: 'q') }
-    locator(:search_button) { button(name: 'btnG') }
+    locator(:search_button) { input(name: 'btnK') }
     locator(:search_results) { ol(id: 'rso') }
 
   end
