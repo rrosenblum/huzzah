d39809fe40b3293de40e4d9a9d899421b30fbb72
Ryan Rosenblum <ryan.rosenblum@gmail.com>
2020-10-01 09:54:07 -0400

    Fix the selector used to search on Bing in tests

--- spec/examples/pages/bing/home_page.rb
+++ spec/examples/pages/bing/home_page.rb
@@ -1,8 +1,6 @@
 module Bing
   class HomePage < Huzzah::Page
-
     locator(:search_box)     { text_field(id: 'sb_form_q') }
-    locator(:search_button)  { button(id: 'sb_form_go') }
-
+    locator(:search_button)  { label(class: 'search icon tooltip') }
   end
 end
